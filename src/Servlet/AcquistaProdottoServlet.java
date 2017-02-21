e2b36eff735f1ee23a46c91e77f71fbf1512db02
Francesco182g <francescogarofalo182@live.com>
2017-02-21 17:29:39 +0100

    Verifica acquisto prodotto

--- src/Servlet/AcquistaProdottoServlet.java
+++ src/Servlet/AcquistaProdottoServlet.java
@@ -91,6 +91,7 @@ public class AcquistaProdottoServlet extends HttpServlet {
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+				System.out.println("ERRORE INSERIMENTO (Ordine già presente)");
 				request.getRequestDispatcher("MieiOrdini.jsp").forward(request, response);
 
 			}
