9e29b589745f50f446992ca2026d13458c4f6377
Antonio Fasulo <antonio.fasulo95@gmail.com>
2018-12-19 17:54:44 +0100

    Ritorno commit

--- src/UnitTest/GestioneUtenteTest.java
+++ src/UnitTest/GestioneUtenteTest.java
@@ -12,39 +12,40 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class GestioneUtenteTest {
-	
-	//Dichiarazione delle variabili di istanza che permettono di controllare se il valore è presente all'interno del sistema
-	private static final String email= "Francesco@Garofalo.com";
-	private static final String nome= "Giovanni";
-	private static final String cognome= "Dario";
-	private static final String password= "Andrea85";
-	private static final String conferma_password= "Andrea85";
-	private static final String sesso= "u";
-	
-	private static Utente utente;
-	//Testo il metodo della registrazione
-	@Test
-	public void check1_Registrazione() throws SQLException {
-		utente= new Utente(email, nome, cognome, password, sesso, "foto");
-		DatabaseQuery.addUser(utente);
-		Utente u = DatabaseQuery.getUtenteByID(email);
-		System.out.println(u.toString());
-		assertEquals(u, utente);
-	}
-	
-	
-	//Metodo che successivamente va a testare la login
-	@Test
-	public void check2_Login() throws SQLException {
-		Utente u = DatabaseQuery.getUtenteByID(email);
-		assertEquals(utente, u);
-	}
-	
-	//Metodo che permette di testare la logout
-	@Test
-	public void check3_Delete() throws SQLException {
-		DatabaseQuery.delUser(email);
-		Utente u = DatabaseQuery.getUtenteByID(email);
-		assertEquals(u, null);
-	}
+  
+  //Dichiarazione delle variabili di istanza che permettono di controllare se il valore è presente all'interno del sistema
+  private static final String email= "Francesco@Garofalo.com";
+  private static final String nome= "Giovanni";
+  private static final String cognome= "Dario";
+  private static final String password= "Andrea85";
+  private static final String conferma_password= "Andrea85";
+  private static final String sesso= "u";
+    
+  private static Utente utente;
+  //Testo il metodo della registrazione
+  @Test
+  public void check1_Registrazione() throws SQLException {
+    utente= new Utente(email, nome, cognome, password, sesso, "foto");
+    DatabaseQuery.addUser(utente);
+    Utente u = DatabaseQuery.getUtenteByID(email);
+    System.out.println(u.toString());
+    assertEquals(u, utente);
+  }
+  
+  
+  //Metodo che successivamente va a testare la login
+  @Test
+  public void check2_Login() throws SQLException {
+    Utente u = DatabaseQuery.getUtenteByID(email);
+    assertEquals(utente, u);
+  }
+  
+  //Metodo che permette di testare la logout
+  @Test
+  public void check3_Delete() throws SQLException {
+    DatabaseQuery.delUser(email);
+    Utente u = DatabaseQuery.getUtenteByID(email);
+    assertEquals(u, null);
+  }
+
 }
