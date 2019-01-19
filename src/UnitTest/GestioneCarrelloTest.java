221b05593a0d3b27b66457eabc8e86dbcffaebea
Antonio Fasulo <antonio.fasulo95@gmail.com>
2019-01-19 10:07:30 +0100

    Modifiche Test Unità

--- src/UnitTest/GestioneCarrelloTest.java
+++ src/UnitTest/GestioneCarrelloTest.java
@@ -2,6 +2,7 @@ package UnitTest;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
@@ -20,15 +21,29 @@ public class GestioneCarrelloTest {
 
 	//Dichiarazione delle variabili di istanza che permettono di controllare se il valore è presente all'interno del sistema
 	private static final String email= "Francesco@Garofalo.com";
+	private static final String email1= "Antonio@Fasulo.com";
 	private static final String nome= "Giovanni";
 	private static final String cognome= "Dario";
 	private static final String password= "Andrea85";
 	private static final String conferma_password= "Andrea85";
 	private static final String sesso= "u";
 	private static Utente utente;
+	private static Utente utente1;
 
 	private static final int id_Carrello = 1;
-	private static final int id_Prodotto = 5;
+
+	//Variabili di istanza per un determinato prodotto
+	private static final int id_prodotto = 80;
+	private static final String descrizione = "Prodotto usato per il test";
+	private static final int quantità= 3;
+	BigDecimal prezzo = new BigDecimal(30.00);
+	private static final String tipo = "test tipo";
+	private static final String condizione = "usato";
+	private static final String nomeProd = "Tazza Napoli";
+	private static final String path= "test path";
+	//private static final String email= "andrea@bocelli.it";
+	private static final int offerta= 20;
+	private static Prodotto p = new Prodotto();
 
 	/*
 	 * Metodo che permette di testare sia l'aggiunta che il ritorno di un prodotto da un determinato carrello
@@ -36,12 +51,30 @@ public class GestioneCarrelloTest {
 	@Test
 	public void check1_AddCarrello() throws SQLException {
 		boolean check = false;
+		//Inserisco l'utente fittizio
 		utente= new Utente(email, nome, cognome, password, sesso, "foto");
+		utente1= new Utente(email1, nome, cognome, password, sesso, "foto");
 		DatabaseQuery.addUser(utente);
-		DatabaseQuery.addCarrello(id_Prodotto, email);
-		ArrayList<Carrello> prodotti = DatabaseQuery.getCarrello(email);
+		DatabaseQuery.addUser(utente1);
+		//Inserisco il prodotto
+		p.setIdProdotto(id_prodotto);
+		p.setDescrizione(descrizione);
+		p.setQuantità(quantità);
+		p.setPrezzo(prezzo);
+		p.setTipo(tipo);
+		p.setCondizione(condizione);
+		p.setNome(nomeProd);
+		p.setPath(path);
+		p.setIdUtente(email);
+		p.setOfferta(offerta);
+		
+		DatabaseQuery.addProdotto(p, email);
+		
+		DatabaseQuery.addCarrello(p.getIdProdotto(), email1);
+		
+		ArrayList<Carrello> prodotti = DatabaseQuery.getCarrello(email1);
 		for (Carrello c: prodotti) {
-			if (c.getIdProdotto() == id_Prodotto) {
+			if (c.getIdProdotto() == p.getIdProdotto()) {
 				check = true;
 			}
 		}
@@ -55,12 +88,12 @@ public class GestioneCarrelloTest {
 	public void check2_DeleteCarrelloProdotto () throws SQLException {
 		boolean check = true;
 		System.out.println("Eccomi");
-		DatabaseQuery.delProdottoCarrello(id_Prodotto);
+		DatabaseQuery.delProdottoCarrello(p.getIdProdotto());
 		System.out.println("Mi trovo qui");
-		ArrayList<Carrello> prodotti = DatabaseQuery.getCarrello(email);
+		ArrayList<Carrello> prodotti = DatabaseQuery.getCarrello(email1);
 		System.out.println("Stampo i valori di ritorno" + prodotti);
 		for (Carrello p: prodotti) {
-			if (p.getIdProdotto() == id_Prodotto) {
+			if (p.getIdProdotto() == p.getIdProdotto()) {
 				check = false;
 			}
 		}
@@ -70,11 +103,13 @@ public class GestioneCarrelloTest {
 	@Test
 	public void check3_CancelloCarrello () throws SQLException {
 
+		DatabaseQuery.delUser(email1);
 		DatabaseQuery.delUser(email);
+		DatabaseQuery.delProdotto(p.getIdProdotto());
 
-		DatabaseQuery.delCarrello(email);
+		DatabaseQuery.delCarrello(email1);
 
-		ArrayList<Carrello> carrelli = DatabaseQuery.getCarrello(email);
+		ArrayList<Carrello> carrelli = DatabaseQuery.getCarrello(email1);
 
 		assertEquals(carrelli.size(), 0);
 
